1b4840147e082cb18a116b9b1dabfae1956ec3e2
unknown <rasmus.carlsen@web.de>
2020-06-12 03:26:38 +0200

    Forgot to add new peros.py and peros_gen.py

---
+++ peros.py
@@ -0,0 +1,91 @@
+from discord.ext import commands
+import sqlite3
+import re
+import datetime
+import threading
+import asyncio
+import json
+from collections import defaultdict
+
+""" Module for counting peros channel-specific """
+
+"""
+Requires peros_channels.json, which contains a list of channel ids, where peros will be available.
+
+Example:
+[
+	2134567812312,
+	9876544444443
+]
+
+"""
+
+emoteregex = re.compile(r"(?:[^\\]|\\\\|^)(<:[A-Za-z0-9]+:\d{9,}>)")
+perosargs = re.compile(r"(?:.+peros )(.*)")
+
+def getEmoteName(emoteTag):
+	return emoteTag.split(":")[1]
+
+class Peros(commands.Cog):
+
+	def __init__(self, bot):
+		self.client = bot
+		self.buxman = bot.buxman
+		
+		with open("peros_channels.json", "r") as f:
+			self.channels = json.load(f)
+
+	@commands.command()
+	async def peros(self, ctx):
+		args = perosargs.match(ctx.message.content)
+		if not args is None:
+			args = args.group(1)
+		else:
+			args = ""
+		
+		chid = ctx.channel.id
+		uid = ctx.author.id
+		
+		if args == "here" or args == "h":
+			if not chid in self.channels:
+				await ctx.channel.send("Peros are not tracked in this channel.")
+				return
+			if not self.buxman.perobux_exists(uid, chid):
+				self.buxman.set_perobux(uid, chid, 0)
+			
+			await ctx.channel.send("<@{0}>, You currently have {1} perobux in this channel!".format(ctx.author.id, self.buxman.get_perobux(uid, chid)))
+			return
+		
+		await ctx.channel.send("<@{0}>, You currently have {1} perobux!".format(ctx.author.id, self.buxman.get_perobux_for_channels(uid, self.channels)))
+
+	async def on_reaction(self, payload, isAdd):
+		amount = 1 if isAdd else -1
+		# Get user id and channel id
+		uid = payload.user_id
+		chid = payload.channel_id
+		if not chid in self.channels:
+			return
+		
+		channel = self.client.get_channel(chid)
+		msg = await channel.fetch_message(payload.message_id)
+		
+		# Check if peroentry exists. If not, do nothing
+		if not self.buxman.peros_exists(uid, chid):
+			if not isAdd:
+				return
+			self.buxman.set_peros(uid, chid, 0)
+		
+		# Check that it is poipero
+		if payload.emoji.name.lower() == "poipero":
+			self.buxman.adjust_peros(uid, chid, amount)
+
+	@commands.Cog.listener()
+	async def on_raw_reaction_add(self, payload):
+		await self.on_reaction(payload, True)
+
+	@commands.Cog.listener()
+	async def on_raw_reaction_remove(self, payload):
+		await self.on_reaction(payload, False)
+
+def setup(bot):
+	bot.add_cog(Peros(bot))
---
+++ peros_gen.py
@@ -0,0 +1,78 @@
+import discord
+from discord.ext import commands
+import json
+import datetime
+from sys import exit, argv
+from user_stats import Manager
+import math
+
+"""
+Requires the same files as snek2.py
+Additionally needs: peros_gen_channels.json
+which contains the channels, which will be calculated when init_peros is run
+"""
+
+prefixes = ["?", "Snek ", "snek ", "SNEK "]   # note trailing space in name prefix
+settings = {}  # overwritten by loading functions below
+
+if len(argv) > 1 and argv[1] == "-t":
+	with open("settings_testing.json", "rb") as f:
+		settings = json.load(f)
+	bot = commands.Bot(command_prefix=["!"])  # testing bot only responds to !-prefixed cmds
+
+else:
+	with open("settings.json", "rb") as f:
+		settings = json.load(f)
+	bot = commands.Bot(command_prefix=prefixes)
+
+channels = []
+with open("peros_gen_channels.json", "r") as f:
+	channels = json.load(f)
+
+def is_owner(ctx):
+	return ctx.message.author.id == bot.settings["owner_id"]
+
+@bot.command()
+@commands.check(is_owner)
+async def init_peros(ctx):
+	await ctx.channel.send("Started calculating peros for {0} channels".format(len(channels)))
+	starttime = datetime.datetime.now()
+	msgcount = 0
+	""" For each channel in peros_gen_channels, look for poipero reaction and adjust entry for uid, chid """
+	for chid in channels:
+		channel = bot.get_channel(chid)
+		msgcount += await compute_peros(channel)
+	
+	dt = datetime.datetime.now() - starttime 
+	timestr = ""
+	timenames = ("days", "hours", "minutes", "seconds")
+	timevalues = (dt.days, math.floor(dt.seconds / 3600), math.floor(dt.seconds / 60), dt.seconds % 60)
+	for i, _ in enumerate(timenames):
+		if timevalues[i] != 0:
+			timestr += "{0:.0f} {1} ".format(timevalues[i], timenames[i])
+	await ctx.channel.send("Finished calculating peros for {0} messages in {1}".format(msgcount, timestr))
+
+""" Compute perobucks. If start is none, do inital compute """
+async def compute_peros(channel):
+	""" Get history in 200 message batches """
+	oldest_message = None
+	ch = channel.id
+	bot.buxman.set_all_peros(ch, 0)
+	msgcount = 0
+	while True:
+		msgs = await channel.history(limit=200,before = oldest_message, oldest_first = False).flatten()
+		if not msgs:
+			return msgcount
+		msgcount += len(msgs)
+		for msg in msgs:
+			uid = msg.author.id
+			oldest_message = msg
+			""" check for peros """
+			peros = list(filter(lambda r: r.emoji.name.lower() == "poipero", filter(lambda r: not isinstance(r.emoji, str), msg.reactions)))
+			if peros:
+				bot.buxman.adjust_peros(uid, ch, peros[0].count)
+
+bot.buxman = Manager(bot)
+bot.settings = settings
+
+bot.run(bot.settings["client_secret"])
