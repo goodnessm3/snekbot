0caf65ed7e03f63a53aab68f1df76131c563f5a1
goodnessm3 <cthulhu3125@gmail.com>
2021-02-20 14:30:54 +0000

    twitter checker - use async methods to reschedule call, fix channel id reading from file

--- niko.py
+++ niko.py
@@ -1,7 +1,6 @@
-from  tweepy import OAuthHandler
+from tweepy import OAuthHandler
 import tweepy
 import asyncio
-import discord
 from discord.ext import commands
 import json
 
@@ -20,57 +19,67 @@ with open("twitter_keys.json", "r") as f:
         TI.__setattr__(k, v)  # this is needlessly complicated
 
 
-class twitterlistener(commands.Cog):
-    def __init__(self,bot):
+class TwitterListener(commands.Cog):
+
+    def __init__(self, bot):
+
         self.bot = bot
         auth = OAuthHandler(TI.A, TI.B)
         auth.set_access_token(TI.C, TI.D)
         self.client = tweepy.API(auth)
-        self.chanel_list = []
+        self.channel_list = []
+        self.latest_tweet = None
         try:
             with open("twitter_channels.txt", "r") as f:
                 for line in f.readlines():
                     cid = line.rstrip("\n")
-                    self.chanel_list.append(self.bot.get_channel(cid))
+                    # bot.get_channel will just return None if you give it a string
+                    self.channel_list.append(cid)
         except FileNotFoundError:
             pass
 
-        bot.loop.create_task(self.get_tweet())
+        self.bot.loop.call_later(10, lambda: asyncio.ensure_future(self.get_tweet()))
+        # wait 10 seconds until bot is logged in
 
     async def get_tweet(self):
-        latest_tweet = None
-        while True:
+
+        print(self.channel_list)
+        tweet = self.client.user_timeline("3096462845", count=1)[0]
+
+        if tweet.in_reply_to_status_id is None and tweet.id != self.latest_tweet:
+            for q in self.channel_list:
+                i = self.bot.get_channel(q)
+                await i.send("https://twitter.com/"+tweet.user.name+"/status/"+str(tweet.id))
+                await i.send("https://tenor.com/view/niko-gif-18543948")
+            self.latest_tweet = tweet.id
+
+        self.bot.loop.call_later(300, lambda: asyncio.ensure_future(self.get_tweet()))
+        # schedule to check again in 5 mins
+
+    @commands.command()
+    async def add_this(self, ctx):
+
+        if str(ctx.message.channel.id) not in self.channel_list:
+            self.channel_list.append(str(ctx.message.channel.id))
+            with open("twitter_channels.txt", "a") as f:
+                f.write(str(ctx.message.channel.id))
+                f.write("\n")
+            await ctx.message.channel.send("Added this channel to the list!")
             tweet = self.client.user_timeline("3096462845", count=1)[0]
-            if tweet.in_reply_to_status_id == None and tweet.id != latest_tweet:
-                for i in self.chanel_list:
-                    await i.send("https://twitter.com/"+tweet.user.name+"/status/"+str(tweet.id))
-                    await i.send("https://tenor.com/view/niko-gif-18543948")
-                latest_tweet = tweet.id
-            await asyncio.sleep(30)
+            await ctx.message.channel.send(
+                "The latest tweet is: https://twitter.com/" + tweet.user.name + "/status/" + str(tweet.id))
+            await ctx.message.channel.send("https://tenor.com/view/niko-gif-18543948")
+        else:
+            await ctx.message.channel.send("This channel is already in the list!")
 
     @commands.command()
-    async def add_this(self,ctx):
-            if (ctx.message.channel not in self.chanel_list):
-                self.chanel_list.append(ctx.message.channel)
-                with open("twitter_channels.txt", "a") as f:
-                    f.write(str(ctx.message.channel.id))
-                    f.write("\n")
-                await ctx.message.channel.send("Added this channel to the list!")
-                tweet = self.client.user_timeline("3096462845", count=1)[0]
-                await ctx.message.channel.send(
-                    "The latest tweet is: https://twitter.com/" + tweet.user.name + "/status/" + str(tweet.id))
-                await ctx.message.channel.send("https://tenor.com/view/niko-gif-18543948")
-            else:
-                await ctx.message.channel.send("This channel is already in the list!")
+    async def niko_moment(self, ctx):
 
-    @commands.command()
-    async def niko_moment(self,ctx):
         tweet = self.client.user_timeline("3096462845", count=1)[0]
         await ctx.message.channel.send(
             "The latest niko tweet is: https://twitter.com/" + tweet.user.name + "/status/" + str(tweet.id))
         await ctx.message.channel.send("https://tenor.com/view/niko-gif-18543948")
 
 
-
 def setup(bot):
-    bot.add_cog(twitterlistener(bot))
+    bot.add_cog(TwitterListener(bot))
