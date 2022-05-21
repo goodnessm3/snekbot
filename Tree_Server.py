0a407890d37ff7f27c533c940341979a28b7e03a
miningmanna <>
2022-05-22 00:28:39 +0200

    Made Tree_Server async

--- Tree_Server.py
+++ Tree_Server.py
@@ -4,19 +4,26 @@ import random
 import asyncio
 
 async def recv_int(conn):
-    return socket.ntohl(int.from_bytes(conn.recv(4),sys.byteorder))
+    loop = asyncio.get_running_loop()
+    bytes = await loop.sock_recv(conn, 4)
+    return socket.ntohl(int.from_bytes(bytes, sys.byteorder))
 
 async def recv_str(conn):
-    len =(await recv_int(conn))
-    return conn.recv(len).decode()
+    loop = asyncio.get_running_loop()
+    len = await recv_int(conn)
+    bytes = await loop.sock_recv(conn, len)
+    return bytes.decode()
 
 async def send_int(conn,val):
-    conn.send(socket.htonl(val).to_bytes(4,sys.byteorder))
+    loop = asyncio.get_running_loop()
+    bytes = socket.htonl(val).to_bytes(4,sys.byteorder)
+    await loop.sock_sendall(conn, bytes)
 
 async def send_str(conn,val):
+    loop = asyncio.get_running_loop()
     encval = val.encode()
     await send_int(conn,len(encval))
-    conn.send(encval)
+    await loop.sock_sendall(conn, encval)
 
 async def send_list(conn,list):
     await send_int(conn,len(list))
@@ -25,24 +32,25 @@ async def send_list(conn,list):
 
 class Tree_Server:
     def __init__(self , Adress , Port):
-            self.addr = Adress
-            self.port = Port
-            self.full_addr = (self.addr,self.port)
-            sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            random.seed()
+        self.addr = Adress
+        self.port = Port
+        self.full_addr = (self.addr,self.port)
+        random.seed()
 
     async def add_tag(self,tags):
-            sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            sock.connect(self.full_addr)
-            await send_int(sock,1)
-            await send_list(sock,tags)
-            sock.close()
+        loop = asyncio.get_running_loop()
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        await loop.sock_connect(sock, self.full_addr)
+        await send_int(sock,1)
+        await send_list(sock,tags)
+        sock.close()
 
 
 
     async def get_nr_tags(self):
+        loop = asyncio.get_running_loop()
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(self.full_addr)
+        await loop.sock_connect(sock, self.full_addr)
         await send_int(sock, 3)
         val = (await recv_int(sock))
         sock.close()
@@ -50,17 +58,16 @@ class Tree_Server:
 
 
     async def get_random_tag(self):
-
-
-            len = (await self.get_nr_tags())
-            position = random.randint(1,len)
-
-
-            sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-            sock.connect(self.full_addr)
-            await send_int(sock,2)
-            await send_int(sock,position)
-            resp = (await recv_str(sock))
-            sock.close()
-            return resp
+        loop = asyncio.get_running_loop()
+        
+        len = (await self.get_nr_tags())
+        position = random.randint(1,len)
+        
+        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        await loop.sock_connect(sock, self.full_addr)
+        await send_int(sock,2)
+        await send_int(sock,position)
+        resp = await recv_str(sock)
+        sock.close()
+        return resp
 
